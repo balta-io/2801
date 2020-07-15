d23998d252ef6f4c7e731916c828bf8ce0c6ec79
Andre Baltieri <andrebaltieri@hotmail.com>
2020-07-15 13:53:31 -0300

    Finalizado modulo 12

---
+++ Modulo 12 - Moedas/Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10536.25m;
+        }
+    }
+}
